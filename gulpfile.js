870d9c0a8dbd3f4722b912664500f122008562d7
Arsen PC <tatarin08082002@gmail.com>
2026-01-12 14:43:43 +0300

    added universal-solution

--- gulpfile.js
+++ gulpfile.js
@@ -85,8 +85,8 @@ function watching() {
   watch(["app/scss/**/*.scss", ], styles);
   watch(["app/images/_src/"], images);
   watch(["app/js/_src/*.js"], scripts);
-  watch(["app/html/**/*.html"], pages);
-  watch(["app/**/*.html"]).on("change", browserSync.reload);
+  watch(["app/html/**/*.html", "app/images/icons/*.svg"], pages);
+  watch(["app/**/*.html", "app/images/icons/*.svg"]).on("change", browserSync.reload);
 }
 
 function cleanDist() {
