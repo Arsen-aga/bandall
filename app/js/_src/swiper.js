e2c0cd11606c060c50d826cb38cec7a9e2a87f7c
Arsen PC <tatarin08082002@gmail.com>
2026-01-13 18:05:22 +0300

    added support block

--- app/js/_src/swiper.js
+++ app/js/_src/swiper.js
@@ -81,176 +81,52 @@ function updateHighlight(swiper) {
   }
 }
 
-
-
-new Swiper(".dream-interiors__swiper", {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 30,
-  initialSlide: 1,
-  centeredSlides: true,
-  mousewheel: {
-    enabled: true,
-    eventsTarget: ".dream-interiors__swiper", // или 'container'
-    forceToAxis: true, // только горизонтальная прокрутка
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true,
-    // Добавляем обработчик drag для скроллбара
-    dragClass: "swiper-scrollbar-drag",
-    hide: false,
-    snapOnRelease: true,
-  },
-  // Включаем навигацию для тачпада
-  touchEventsTarget: "container",
-});
-new Swiper(".fixed-cost__swiper", {
+new Swiper(".support__swiper", {
   loop: false,
-  slidesPerView: "auto",
-  spaceBetween: 20,
-  mousewheel: {
-    enabled: true,
-    eventsTarget: ".fixed-cost__swiper", // или 'container'
-    forceToAxis: true, // только горизонтальная прокрутка
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true,
-    // Добавляем обработчик drag для скроллбара
-    dragClass: "swiper-scrollbar-drag",
-    hide: false,
-    snapOnRelease: true,
-  },
-  touchEventsTarget: "container",
-  breakpoints: {
-    991.8: {
-      initialSlide: 1,
-    },
-  },
-});
-new Swiper(".video-review__swiper", {
-  loop: true,
-  slidesPerView: "auto",
-  spaceBetween: 18,
-  initialSlide: 2,
-  centeredSlides: true,
-  mousewheel: {
-    enabled: true,
-    eventsTarget: ".video-review__swiper", // или 'container'
-    forceToAxis: true, // только горизонтальная прокрутка
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true,
-    // Добавляем обработчик drag для скроллбара
-    dragClass: "swiper-scrollbar-drag",
-    hide: false,
-    snapOnRelease: true,
-  },
-  on: {
-    slideChange: function (swiper) {
-      swiper.slides.forEach((slide) => {
-        const overlay = slide.querySelector(".overlay");
-        overlay.style.display = "block";
-      });
-      const activeSlideIndex = swiper.activeIndex;
-      const activeSlide = swiper.slides[activeSlideIndex];
-      const overlay = activeSlide.querySelector(".overlay");
-      overlay.style.display = "none";
-    },
-  },
-
-  touchEventsTarget: "container",
-});
-new Swiper(".interior__swiper", {
-  loop: true,
   slidesPerView: 1,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".interior__next",
-    prevEl: ".interior__prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  mousewheel: {
-    enabled: true,
-    eventsTarget: ".interior__swiper", // или 'container'
-    forceToAxis: true, // только горизонтальная прокрутка
-  },
-  // Включаем навигацию для тачпада
-  touchEventsTarget: "container",
-});
-
-new Swiper(".package-documentation__swiper", {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 20,
-  mousewheel: {
-    enabled: true,
-    eventsTarget: ".package-documentation__swiper", // или 'container'
-    forceToAxis: true, // только горизонтальная прокрутка
-  },
-  // Включаем навигацию для тачпада
-  touchEventsTarget: "container",
-});
-new Swiper(".fixing__swiper", {
-  loop: false,
-  slidesPerView: "auto",
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".fixing__next",
-    prevEl: ".fixing__prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  mousewheel: {
-    enabled: true,
-    eventsTarget: ".fixing__swiper", // или 'container'
-    forceToAxis: true, // только горизонтальная прокрутка
-  },
-  // Включаем навигацию для тачпада
-  touchEventsTarget: "container",
-  breakpoints: {
-    991.8: {
-      spaceBetween: 20,
-      slidesPerView: "auto",
-    },
-    575.98: {
-      slidesPerView: 15,
-      slidesPerView: "auto",
-    },
-  },
-});
-new Swiper(".team__swiper", {
-  loop: false,
-  slidesPerView: "auto",
   spaceBetween: 15,
-  initialSlide: 2,
-  centeredSlides: true,
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true,
-    // Добавляем обработчик drag для скроллбара
-    dragClass: "swiper-scrollbar-drag",
-    hide: false,
-    snapOnRelease: true,
+  navigation: {
+    nextEl: ".support__swiper-wrapper .swiper-button-next",
+    prevEl: ".support__swiper-wrapper .swiper-button-prev",
   },
   mousewheel: {
     enabled: true,
-    eventsTarget: ".team__swiper", // или 'container'
+    eventsTarget: ".support__swiper", // или 'container'
     forceToAxis: true, // только горизонтальная прокрутка
   },
   // Включаем навигацию для тачпада
-  touchEventsTarget: "container",
-  breakpoints: {
+    breakpoints: {
+    767.98: {
+      slidesPerView: 2,
+    },
     991.8: {
-      spaceBetween: 20,
-      slidesPerView: "auto",
+      slidesPerView: 3,
+    },
+  },
+  touchEventsTarget: "container",
+  on: {
+    init: function () {
+      updateHighlight(this);
+    },
+    resize: function () {
+      updateHighlight(this);
+    },
+    slideChangeTransitionStart: function () {
+      let lastTimestamp = null;
+      const updateDuringTransition = (timestamp) => {
+        if (!lastTimestamp) lastTimestamp = timestamp;
+        const progress = timestamp - lastTimestamp;
+
+        updateHighlight(this);
+
+        if (progress < 300) {
+          requestAnimationFrame(updateDuringTransition);
+        }
+      };
+      requestAnimationFrame(updateDuringTransition);
+    },
+    slideChangeTransitionEnd: function () {
+      updateHighlight(this);
     },
   },
 });
